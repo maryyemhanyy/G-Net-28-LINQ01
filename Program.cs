27cacc770788c0f8319cbc55864624276017f212
maryem <maryyemhanyy@gmail.com>
2026-04-02 04:03:12 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LINQSession1
+﻿using static LINQSession1.Source;
+
+
+namespace LINQSession1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Q1
+            //Get all products from the "Seafood" category. Print each product's name and price
+            
+            //var result = ProductList.Where(p => p.Category == "Seafood")
+            //                        .Select(p => new { Name = p.ProductName, Price = p.UnitPrice });
+
+            //foreach (var item in result) {
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
